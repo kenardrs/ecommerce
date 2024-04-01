62ebd865f0426098e825375d07227d094323a4e0
F6075077 Kenard Rodrigues da Silva <kenardrs@gmail.com>
2024-04-01 20:25:46 -0300

    monolithic version - final course 1

--- src/main/java/br/com/alura/ecommerce/FraudDetectorsService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorsService.java
@@ -2,19 +2,24 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorsService {
 
     public static void main(String[] args) {
-        var fraudDetectorService = new FraudDetectorsService();
-        try (var service = new KafkaService(
+        var fraudService = new FraudDetectorsService();
+        try (var service = new KafkaService<>(
                 FraudDetectorsService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudDetectorService::parse)) {
+                fraudService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("-------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,18 +1,24 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (int i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,7894589745";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+                for (int i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
 
-                var email = "Thanks for your order, we are finishing processing";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = new Email("Order Done","Thanks for your order, we are finishing processing");
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
